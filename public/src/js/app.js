68acf3407257da962da5af17536afa8401f243fb
Nedu Robert <nedumrobert@gmail.com>
2018-07-01 22:16:38 +0100

    Implemented PWA

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,4 +1,9 @@
 const url = `https://free.currencyconverterapi.com/api/v5`;
+let networkDataReceived = false;
+
+if(!window.Promise) {
+    window.Promise = Promise;
+}
 
 window.onload = () => {
     let fromCurrency = document.getElementById('fromCurrency');
@@ -6,14 +11,62 @@ window.onload = () => {
     let currencyAmount = document.getElementById('currencyAmount');
     let convertedCurrency = document.getElementById('convertedCurrency');
     let convertCurrency = document.getElementById('submitForm');
+    
     convertCurrency.addEventListener('submit', convertCurrencies);
+    
+    registerServiceWorker();
     populateCurrencies();
+}
+
+// register service worker
+const registerServiceWorker =  async () => {
+    if ('serviceWorker' in navigator) {
+            navigator.serviceWorker
+            .register('/sw.js')
+            .then(() => {
+                console.log('Service worker registered!');
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    } else {
+        console.log('Your browser does not support Service Worker.');
+    }
 }
 
 // fetch list of currencies from the api
 async function getCurrencies () {
-    const res = await fetch(`${url}/currencies`);
-    const data = await res.json();
+    const res = await fetch(`${url}/currencies`).catch(() => {
+        // fetch from indexedDB 
+        if ('indexedDB' in window) {
+            readAllData('currencies')
+                .then(function (data) {
+                    const currencies = [];
+                    for (let value in data) {
+                        const currency = value.data.id;
+                        currencies.push(currency);
+                    }
+                    // if (!networkDataReceived) {
+                    console.log('idb: ', currencies);
+                    return currencies;
+                    // }
+                });
+        }
+    });
+    const data = await res.json().catch(() => {
+        // fetch from indexedDB 
+        if ('indexedDB' in window) {
+            readAllData('currencies')
+                .then((data) => {
+                    // if (!networkDataReceived) {
+                        return data;
+                    // }
+                });
+        }
+    });
+    console.log(data);
+    networkDataReceived = true;
+
     return Object.keys(data.results);
 }
 
@@ -21,6 +74,7 @@ async function getCurrencies () {
 async function populateCurrencies () {
     const results = await getCurrencies();
     results.sort();
+    console.log('results', results);
     for(index in results) {
         fromCurrency.options[fromCurrency.options.length] = new Option(results[index], results[index]);
         toCurrency.options[toCurrency.options.length] = new Option(results[index], results[index]);
@@ -38,8 +92,29 @@ async function convertCurrencies (e) {
     const currencyTo = toCurrency.value;
     const query = `${currencyFrom}_${currencyTo},${currencyTo}_${currencyFrom}`;
     
-    const res = await fetch(`${url}/convert?q=${query}`);
-    const data = await res.json();
+    const res = await fetch(`${url}/convert?q=${query}`).catch(() => {
+        // fetch from indexedDB 
+        if ('indexedDB' in window) {
+            readAllData('exchangeRates')
+                .then(function (data) {
+                    if (!networkDataReceived) {
+                        convertedCurrency.value = (data.results[0].exchange_rate * Number(amount)).toFixed(3);
+                    }
+                });
+        }
+    });
+    const data = await res.json().catch(() => {
+        // fetch from indexedDB 
+        if ('indexedDB' in window) {
+            readAllData('exchangeRates')
+                .then(function (data) {
+                    if (!networkDataReceived) {
+                        convertedCurrency.value = (data.results[0].exchange_rate * Number(amount)).toFixed(3);
+                    }
+                });
+        }
+    });;
+    console.log('web: ', data);
 
     convertedCurrency.value = (data.results[`${currencyFrom}_${currencyTo}`].val * Number(amount)).toFixed(3);
 }
